cf922e2ad2fc9ca36d1f6d5d70c8057f1f7c908b
Celestial Gardener <ericdelimaomega@gmail.com>
2026-02-26 20:51:08 -0300

    Add files via upload

---
+++ lista01_03.py
@@ -0,0 +1,12 @@
+altura : int =0
+base : int =0
+area : int = 0
+
+altura = int(input('entre a altura'))
+
+
+base = int(input('entre a base'))
+
+area = (base*altura)/2
+
+print('sua area é:',area)
