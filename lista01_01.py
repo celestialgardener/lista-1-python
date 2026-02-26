02199c46279e9fd339c8078329b54c7e7fea80ea
Celestial Gardener <ericdelimaomega@gmail.com>
2026-02-26 19:42:21 -0300

    Add files via upload

---
+++ lista01_01.py
@@ -0,0 +1,6 @@
+#coletar o valor de um lado de um quadrado e achar sua area
+lado : int = 0
+area : int = 0
+lado = int(input('digite o valor do lado'))
+area = lado * lado
+print('a area do seu quadrado é: ',area)
