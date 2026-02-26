cfd673869f68e26dcada0cdbe12bf6ca24d7f29b
Celestial Gardener <ericdelimaomega@gmail.com>
2026-02-26 20:11:28 -0300

    Add files via upload

---
+++ lista01_02.py
@@ -0,0 +1,8 @@
+#recer salario e ajustar 15%
+salario1 : int = 0
+salario2 : float =0 
+salario1 = int(input('me diga o salario atual'))
+
+salario2 = ( salario1 + salario1 * 0.15)
+
+print('o salario ajustado é de: ',salario2)
